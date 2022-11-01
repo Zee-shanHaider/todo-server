542cd801ba48e0acdc12329c54167794eb6ad4e5
Zeeshan Haider <zeeshan9402529@gmail.com>
2022-11-01 22:40:44 +0500

    set cors

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ const fileStorage=multer.diskStorage({
 app.use(express.json())
 
 app.use((req,res,next)=>{
-    res.setHeader('Access_Control_Allow_Origin', '*');
+    res.set('Access_Control_Allow_Origin', '*');
     res.setHeader('Access_Control_Allow_Methods', 'GET, PUT, POST, DELETE, PATCH');
     res.setHeader('Access_Control_Allow_Headers', 'Content_Type, Authorization');
     next()
