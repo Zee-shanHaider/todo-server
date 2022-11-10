ece1364677e8d51753a4299086e420eae28df74d
Zeeshan Haider <zeeshan9402529@gmail.com>
2022-11-10 16:53:44 +0500

    adding again helmet with crossOriginPolicy

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use((req,res,next)=>{
 })
 app.use(cors())
 
-// app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
 const mongoose = require('mongoose');
 
